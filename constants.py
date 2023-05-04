e7e30571f5489de8c6eb965b139b747c77760e65
HelloWorld <qb3053136@hotmail.com>
2023-05-04 19:28:51 +0800

    Create constants.py

---
+++ constants.py
@@ -0,0 +1,12 @@
+header = {
+    "content-type": "application/json",
+    "refer": "https://www.bing.com",
+    "origin": "https://www.bing.com",
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36 Edg/110.0.1587.50"
+}
+
+base_url = "https://www.bing.com"
+
+create_conversation = "/turing/conversation/create"
+
+end_of_stream = ''
